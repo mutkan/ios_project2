4fbf463283ef11bb90511be9ee95957c8303a083
mutlu <mutkanx@gmail.com>
2020-04-30 22:53:52 -0500

    First commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Mutlu Celep on 30.04.2020.
+//  Copyright © 2020 Mutlu Celep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
