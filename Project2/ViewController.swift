58a2a7d703e4dc2dbd22c2722ca8040fb3eeabf6
mutlu <mutkanx@gmail.com>
2020-05-02 23:36:58 -0500

    Add random country name to title

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,9 +34,12 @@ class ViewController: UIViewController {
     }
     
     func askQuestion(){
+        countries.shuffle()
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        correctAnswer = Int.random(in: 0...2)
+        title = countries[correctAnswer].uppercased()
     }
 
 
